c968037af5e73bd327df56782ebc32821993858d
tabinks <abinkowski@andrews-mbp.attlocal.net>
2017-02-13 13:54:24 -0600

    Add session 6.

---
+++ Session6.playground/Pages/Parsing HTML.xcplaygroundpage/Contents.swift
@@ -0,0 +1,63 @@
+import Foundation
+import PlaygroundSupport
+import UIKit
+
+//XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+let string = "Washington (CNN) Retired Supreme Court Justice Sandra Day O&#39;Connor says President Barack <b>Obama</b> should name Antonin Scalia&#39;s replacement. O&#39;Connor, a nominee of President Ronald Reagan who became the court&#39;s swing vote until she retired from&nbsp;..."
+print(string)
+
+let label = UILabel(frame: CGRect(x: 0, y: 0, width: 1000, height: 100))
+label.text = string
+label.numberOfLines = 3
+
+///: Use regular expression to strip out the html tags
+///: This leaves some of the markup.
+let regex = try! NSRegularExpression(pattern: "<.*?>", options: [.caseInsensitive])
+let range = NSMakeRange(0, string.characters.count)
+let htmlLessString :String = regex.stringByReplacingMatches(in: string, options: [],
+  range:range ,
+  withTemplate: "")
+print(htmlLessString)
+
+
+///:  Use the html for format the string using `NSAttributedString`.
+var attrStr = try! NSAttributedString(
+  data: string.data(using: String.Encoding.unicode, allowLossyConversion: true)!,
+  options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
+  documentAttributes: nil)
+label.attributedText = attrStr
+
+
+///: # Extension of `String`
+
+extension String {
+  
+  /// Convert HTML to an `NSAtributedString`
+  func html2attributedString() -> NSAttributedString {
+    let attrStr = try! NSAttributedString(
+      data: self.data(using: String.Encoding.unicode, allowLossyConversion: true)!,
+      options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
+      documentAttributes: nil)
+    return attrStr
+  }
+  
+  /// Test if a string contains a string
+  func contains(s: String) -> Bool {
+    return self.range(of:s) != nil ? true : false
+  }
+  /*
+  /// Truncate a string to a given length
+  func trunc(length: Int, trailing: String? = "...") -> String {
+    if self.characters.count > length {
+      return self.substringToIndex(self.startIndex.advancedBy(length)) + (trailing ?? "")
+    } else {
+      return self
+    }
+  }
+ */
+}
+
+label.attributedText = "This is an HTML string.  This word is <b>bold</b>. This contains a '&nbsp;&nbsp;'".html2attributedString()
+
+
